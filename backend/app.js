50bdd968cfcd37a6f9cd2e968914ea0af8863fa1
Vincent Phung <vincent.phung05@gmail.com>
2020-08-14 16:26:05 -0400

    Pokemon data properly recieved

--- backend/app.js
+++ backend/app.js
@@ -31,82 +31,112 @@ app.post('/', (req, res) => {
 });
 
 //An Array of all 807 Pokemon, starting with bulbasaur to zeraora
-let nationalPokedex = new Array(1);
+let nationalPokedex = new Array(2);
 console.log(nationalPokedex.length);
 
-let i;
-for (i = 0; i < nationalPokedex.length; i++) {
-  let pokeID = i + 1;
-  initPokeData(pokeID);
+//This function takes the data and makes an array of the pokemon
+async function generatePokedex() {
+  let i;
+  for (i = 0; i < nationalPokedex.length; i++) {
+    let pokeID = i + 1; //to get pokemon to show up in order make this function async, and put await in front of this line
+    const data1 = await initPokeData1(pokeID);
+    const data2 = await initPokeData2(pokeID);
+    console.log(data1);
+    console.log(data2);
+
+
+  }
 }
 
-//function doing all the calls to pokeapi
-async function initPokeData(pokemonID) {
-  const pokemon = await fetch(
-    `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}/`
-  );
-  const pokeData = await pokemon.json();
+//function which calls to pokeapi and gets the first portion of data required
+async function initPokeData1(pokemonID) {
+  try {
+    const pokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${pokemonID}/`
+    );
+    const pokeData = await pokemon.json();
 
-  let id = pokeData.id;
-  let name = pokeData.name;
-  let genus = pokeData.genera[7].genus;
-  let entry;
+    let id = pokeData.id;
+    let name = pokeData.name;
+    let genus = pokeData.genera[7].genus;
+    let entry;
 
-  //entries vary between games
-  if (id <= 151)
-    //Platinum-gen1
-    entry = pokeData.flavor_text_entries[13].flavor_text;
-  else if (id >= 152 || id <= 251)
-    //Platinum-gen2
-    entry = pokeData.flavor_text_entries[10].flavor_text;
-  else if (id >= 252 || id <= 386)
-    //Emerald-gen3
-    entry = pokeData.flavor_text_entries[2].flavor_text;
-  else if (id >= 387 || id <= 493)
-    //Platinum-gen4
-    entry = pokeData.flavor_text_entries[2].flavor_text;
-  else if (id >= 494 || id <= 649)
-    //BW2-gen5
-    entry = pokeData.flavor_text_entries[5].flavor_text;
-  else if (id >= 650 || id <= 721)
-    //Y-gen6
-    entry = pokeData.flavor_text_entries[14].flavor_text;
-  else if (id >= 722 || id <= 802)
-    //USUM-gen7
-    entry = pokeData.flavor_text_entries[17].flavor_text;
-  else if (id >= 803 || id <= 807)
-    //USUM-gen7
-    entry = pokeData.flavor_text_entries[7].flavor_text;
+    //entries vary between games
+    if (id <= 151)
+      //Platinum-gen1
+      entry = pokeData.flavor_text_entries[13].flavor_text;
+    else if (id >= 152 || id <= 251)
+      //Platinum-gen2
+      entry = pokeData.flavor_text_entries[10].flavor_text;
+    else if (id >= 252 || id <= 386)
+      //Emerald-gen3
+      entry = pokeData.flavor_text_entries[2].flavor_text;
+    else if (id >= 387 || id <= 493)
+      //Platinum-gen4
+      entry = pokeData.flavor_text_entries[2].flavor_text;
+    else if (id >= 494 || id <= 649)
+      //BW2-gen5
+      entry = pokeData.flavor_text_entries[5].flavor_text;
+    else if (id >= 650 || id <= 721)
+      //Y-gen6
+      entry = pokeData.flavor_text_entries[14].flavor_text;
+    else if (id >= 722 || id <= 802)
+      //USUM-gen7
+      entry = pokeData.flavor_text_entries[17].flavor_text;
+    else if (id >= 803 || id <= 807)
+      //USUM-gen7
+      entry = pokeData.flavor_text_entries[7].flavor_text;
 
-  p.getPokemonByName(name)
-    .then((res) => {
-      //hectograms to kilograms
-      let weight = res.weight * 0.1;
-      //decimeters to meters
-      let height = res.height * 0.1;
-      let type = res.types;
-      let sprite = res.sprites.front_default;
-      let allInfo = {
-        id,
-        name,
-        weight,
-        height,
-        type,
-        sprite,
-        genus,
-        entry,
-      };
-      createPokemonObj(allInfo);
-    })
-    .catch((err) => {
-      console.log('There was an error: ', err);
-    });
+    let dataSet1 = {
+      id,
+      name,
+      genus,
+      entry
+    };
+    return dataSet1;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//gets the second half of pokemon data required weight,height,type,sprite
+async function initPokeData2(pokemonID) {
+  try {
+    const pokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonID}/`
+    );
+
+    const pokeData = await pokemon.json();
+    let weight = pokeData.weight;
+    let height = pokeData.height;
+    let type = pokeData.types;
+    let sprite = pokeData.sprites.front_default;
+    let dataSet2 = {
+      weight,
+      height,
+      type,
+      sprite
+    };
+    return dataSet2;
+
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 //Creates single pokemon objects to be sent back
 function createPokemonObj(pokemonData) {
   //Destructuring
-  const { id, name, weight, height, type, sprite, genus, entry } = pokemonData;
+  const {
+    id,
+    name,
+    weight,
+    height,
+    type,
+    sprite,
+    genus,
+    entry
+  } = pokemonData;
   let newPokemon = new Pokemon(
     id,
     name,
@@ -117,8 +147,10 @@ function createPokemonObj(pokemonData) {
     genus,
     entry
   );
-  console.log(newPokemon.getEntry()); //I have to import the class...
+  //console.log(newPokemon); //I have to import the class...
 }
+
+generatePokedex();
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
